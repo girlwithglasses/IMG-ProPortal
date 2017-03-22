d83a740159edea1c9b2a3fc69384edc69f7ac92e
ialarmedalien <amelia.ireland@gmail.com>
2017-03-22 07:37:02 -0700

    Relative path to logger config

--- webui.cgi/IMG/App/Role/Logger.pm
+++ webui.cgi/IMG/App/Role/Logger.pm
@@ -2,29 +2,17 @@ package IMG::App::Role::Logger;
 
 use parent 'Log::Contextual';
 use Log::Log4perl;
-#use Dancer2;
+use File::Spec::Functions qw( rel2abs catdir catfile );
+use File::Basename qw( dirname basename );
+use feature ':5.16';
 
-############################################################
-# A simple root logger with a Log::Log4perl::Appender::File
-# file appender and output to STDOUT.
-############################################################
+# find the config file
+my $dir = dirname( rel2abs( $0 ) );
+while ( 'webUI' ne basename( $dir ) ) {
+	$dir = dirname( $dir );
+}
 
-# Configuration in a string ...
-my $conf = q(
-	log4perl.rootLogger                = DEBUG, Logfile, Screen
-
-	log4perl.appender.Logfile          = Log::Log4perl::Appender::File
-	log4perl.appender.Logfile.filename = /global/homes/a/aireland/webUI/proportal/log/proportal-log4perl.log
-	log4perl.appender.Logfile.mode     = append
-	log4perl.appender.Logfile.layout   = Log::Log4perl::Layout::PatternLayout
-	log4perl.appender.Logfile.layout.ConversionPattern = [%r] %F %L %c - %m%n
-
-	log4perl.appender.Screen         = Log::Log4perl::Appender::Screen
-	log4perl.appender.Screen.stderr  = 0
-	log4perl.appender.Screen.layout  = Log::Log4perl::Layout::SimpleLayout
-);
-
-Log::Log4perl->init( \$conf );
+Log::Log4perl->init( catfile( $dir, 'proportal/environments', 'logger.conf' ) );
 
 sub arg_default_logger { $_[1] || Log::Log4perl->get_logger }
 
