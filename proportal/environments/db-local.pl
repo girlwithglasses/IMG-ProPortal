768a3f542f703e73b9ecd50bcf05652bcafa40b6
ialarmedalien <amelia.ireland@gmail.com>
2017-04-19 08:44:03 -0700

    Adding db config

---
+++ proportal/environments/db-local.pl
@@ -0,0 +1,21 @@
+{	db => {
+		imgsqlite => {
+			driver => 'SQLite',
+			database => '/global/homes/a/aireland/webUI/proportal/share/dbschema-img_core.db',
+			dbi_params => {
+				RaiseError => 1,
+				FetchHashKeyName => 'NAME_lc',
+			},
+	#		log_queries => 1,
+		},
+		cycog => {
+			driver => 'SQLite',
+			database => '/global/homes/a/aireland/webUI/proportal/share/cycog.db',
+			dbi_params => {
+				RaiseError => 1,
+				FetchHashKeyName => 'NAME_lc',
+			},
+	#		log_queries => 1,
+		}
+	}
+}
